76bdaee1401d05c5df73ff92d091bddd7141dc5a
Vitaliy <vitaliy.fry@gmail.com>
2016-06-16 00:49:45 +0300

    add style for cells

--- app/controllers/profile/inventories_controller.rb
+++ app/controllers/profile/inventories_controller.rb
@@ -4,6 +4,7 @@ class Profile::InventoriesController < ApplicationController
 
   def show
     @profile = current_user.profile
+    @items = Item::Item.where(id: @profile.inventory).decorate
     get_equipment
   end
 end
