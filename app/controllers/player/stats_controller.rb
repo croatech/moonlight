0b4d41f32a95382f0993fd48e4aa0d02b4a65286
Vitaliy <vitaliy.fry@gmail.com>
2016-07-01 19:29:37 +0300

    add images for artifacts

--- app/controllers/player/stats_controller.rb
+++ app/controllers/player/stats_controller.rb
@@ -3,9 +3,12 @@ class Player::StatsController < ApplicationController
   layout 'player'
 
   def index
-    @player = current_user.player
-    set_equipment_items
-    @wearable_tools = get_tool_items
+    @player = current_user.player.decorate
+    wearable_equipment_ids = Player::Inventory::WearableItemsIdsService.new(current_user.player, Player::EQUIPMENT_SLOTS).call
+    @wearable_eqipment = Equipment::Item.where(id: wearable_equipment_ids).includes(:category)
+
+    wearable_tools_ids = Player::Inventory::WearableItemsIdsService.new(current_user.player, Player::TOOL_SLOTS).call
+    @wearable_tools = Tool::Item.where(id: wearable_tools_ids).includes(:category)
   end
 
   def increase
