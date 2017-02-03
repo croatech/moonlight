934a5f0981a872d3e24b74c2bed0c0edadfe69ab
Vitaliy Fry <vitaliy.fry@gmail.com>
2017-02-03 04:04:17 +0300

    start moving to rails 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,20 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
 
   has_one :player
 
   validates :name, presence: true, uniqueness: true
+
+  after_create do
+    default_avatar = Avatar.first
+
+    Player.create(user_id: self.id, current_hp: 20, gold: 1500, name: self.name, avatar_id: default_avatar.id)
+  end
 end
