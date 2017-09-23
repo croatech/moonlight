4752dfda40b3316d2bcf43b24128d26e205a36c5
Vitaliy Fry <vitaliy.fry@gmail.com>
2017-09-23 20:24:29 +0300

    add spec for Api::Equipment::CategoriesController

--- app/controllers/api/equipment/categories_controller.rb
+++ app/controllers/api/equipment/categories_controller.rb
@@ -1,4 +1,6 @@
 class Api::Equipment::CategoriesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     render json: ::Equipment::Category.includes(:items).where(equipment_items: { artifact: false })
   end
