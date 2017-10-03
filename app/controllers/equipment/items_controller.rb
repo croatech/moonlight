7a69dfc1629c32e81977092ea77168ac16c4a3a3
Vitaliy Fry <vitaliy.fry@gmail.com>
2017-10-03 23:27:44 +0300

    change Equipment::Items::PutOnService

--- app/controllers/equipment/items_controller.rb
+++ app/controllers/equipment/items_controller.rb
@@ -1,6 +1,6 @@
 class Equipment::ItemsController < ApplicationController
   def put_on
-    service = Equipment::Items::PutOnService.new(current_user.player, find_item)
+    service = Equipment::Items::PutOnService.new(player: current_player, item: find_item)
     service.call
     redirect_back(fallback_location: root_path)
   end
---
+++ app/services/equipment/items/recalculate_stats_service.rb
@@ -0,0 +1,19 @@
+class Equipment::Items::RecalculateStatsService
+  def self.increase(player, item)
+    Player::STATS.each do |stat|
+      next if stat.nil?
+      player[stat] += item[stat]
+    end
+
+    player.save
+  end
+
+  def self.decrease(player, item)
+    Player::STATS.each do |stat|
+      next if stat.nil?
+      player[stat] += item[stat]
+    end
+
+    player.save
+  end
+end
