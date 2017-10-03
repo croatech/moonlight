4e163ffc36ffb85bd32c6725b0a15c175e80e19b
Vitaliy Fry <vitaliy.fry@gmail.com>
2017-10-03 21:11:51 +0300

    add polymorhpic stuff model for resources, etc.

--- db/migrate/20171003044843_dsf.rb
+++
@@ -1,7 +0,0 @@
-class Dsf < ActiveRecord::Migration[5.1]
-  def change
-    rename_table :players_equipment_items, :player_equipment_items
-    rename_table :players_tool_items, :player_tool_items
-    rename_table :players_resources, :player_resources
-  end
-end
