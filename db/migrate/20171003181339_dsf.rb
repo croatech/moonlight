dfc117ef5a7b455fc5160077f714289fe826dda5
Vitaliy Fry <vitaliy.fry@gmail.com>
2017-10-04 22:51:35 +0300

    remove redundant trash

--- db/migrate/20171003181339_dsf.rb
+++
@@ -1,7 +0,0 @@
-class Dsf < ActiveRecord::Migration[5.1]
-  def change
-    change_column :stuffs, :stuffable_id, :bigint, null: false
-    change_column :stuffs, :player_id, :bigint, null: false
-    change_column :stuffs, :stuffable_type, :string, null: false
-  end
-end
--- db/migrate/20171004010234_change_column.rb
+++
@@ -1,7 +0,0 @@
-class ChangeColumn < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :equipment_categories, :slug, :type
-    rename_column :tool_categories, :slug, :type
-    remove_column :tool_items, :type
-  end
-end
