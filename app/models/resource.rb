bf32de03e5e564684b0f50458f0bf2a77bc3e4c1
Vitaliy Fry <vitaliy.fry@gmail.com>
2017-10-04 22:46:36 +0300

    add other type checking to Stuff::SellService

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -14,6 +14,7 @@
 #
 
 class Resource < ApplicationRecord
+  # TODO change type to category_id and add required_skill
   self.inheritance_column = nil
 
   belongs_to :item, class_name: 'Tool::Item', foreign_key: 'item_id'
---
+++ spec/factories/resource.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :resource do
+    name 'Wood'
+    price 300
+  end
+end
