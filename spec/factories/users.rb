9dcfce10cbe10c291b0b02931b29d3ffc5739603
Vitaliy Fry <vitaliy.fry@gmail.com>
2017-10-05 02:59:26 +0300

    add spec for Stuff::BuyService

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -20,8 +20,8 @@
 
 FactoryGirl.define do
   factory :user do
-    sequence :email {|n| "test-#{n}@gmail.com"}
-    password  'password'
+    sequence(:email) { |n| "test-#{n}@gmail.com" }
+    password 'password'
     name 'Test'
   end
 end
--- spec/services/stuff/buy_service_spec.rb
+++ spec/services/stuff/buy_service_spec.rb
@@ -1,58 +1,112 @@
 require 'spec_helper'
 
-describe Equipment::Items::BuyService do
+describe Stuff::BuyService do
   subject do
-    Equipment::Items::BuyService.new.call(player: player, item: item)
+    Stuff::BuyService.new.call(player: player, item: item)
   end
 
   let(:object) { subject.right }
   let(:errors) { subject.left }
 
-  let(:player) { create(:player, gold: 6, level: 6) }
-  let(:item) { create(:equipment_item, price: 6, required_level: 6) }
+  let(:player) { create(:player, gold: 6, level: 6, lumberjacking_skill: 50) }
 
-  describe 'success' do
-    it 'check that service succeeded' do
-      expect(subject.success?).to eq true
+  describe 'Equipment::Item' do
+    let(:item) { create(:equipment_item, price: 6, required_level: 6) }
+
+    describe 'success' do
+      it 'check that service succeeded' do
+        expect(subject.success?).to eq true
+      end
+
+      it 'check that service has not errors' do
+        expect(errors).to be_nil
+      end
     end
 
-    it 'check that service has not errors' do
-      expect(errors).to be_nil
+    describe 'gold' do
+      context 'not enough' do
+        let(:player) { create(:player, gold: 0, level: 6) }
+
+        it 'check that service failed' do
+          expect(subject.failure?).to eq true
+        end
+
+        it 'check that service has errors' do
+          expect(errors).not_to be_nil
+        end
+
+        it 'checks that service has particular error' do
+          expect(errors).to include('Gold is not enough')
+        end
+      end
+    end
+
+    describe 'level' do
+      context 'not enough' do
+        let(:player) { create(:player, level: 1, gold: 6) }
+
+        it 'check that service failed' do
+          expect(subject.failure?).to eq true
+        end
+
+        it 'check that service has errors' do
+          expect(errors).not_to be_nil
+        end
+
+        it 'checks that service has particular error' do
+          expect(errors).to include('Level or skill is not enough')
+        end
+      end
     end
   end
 
-  describe 'gold' do
-    context 'not enough' do
-      let(:player) { create(:player, gold: 0, level: 6) }
+  describe 'Tool::Item' do
+    let(:category) { create(:tool_category, type: 'lumberjacking') }
+    let(:item) { create(:tool_item, price: 6, required_skill: 6, category: category) }
 
-      it 'check that service failed' do
-        expect(subject.failure?).to eq true
+    describe 'success' do
+      it 'check that service succeeded' do
+        expect(subject.success?).to eq true
       end
 
-      it 'check that service has errors' do
-        expect(errors).not_to be_nil
-      end
-
-      it 'checks that service has particular error' do
-        expect(errors).to include('Gold is not enough')
+      it 'check that service has not errors' do
+        expect(errors).to be_nil
       end
     end
-  end
 
-  describe 'level' do
-    context 'not enough' do
-      let(:player) { create(:player, level: 1, gold: 6) }
+    describe 'gold' do
+      context 'not enough' do
+        let(:player) { create(:player, gold: 0, lumberjacking_skill: 50) }
 
-      it 'check that service failed' do
-        expect(subject.failure?).to eq true
+        it 'check that service failed' do
+          expect(subject.failure?).to eq true
+        end
+
+        it 'check that service has errors' do
+          expect(errors).not_to be_nil
+        end
+
+        it 'checks that service has particular error' do
+          expect(errors).to include('Gold is not enough')
+        end
       end
+    end
 
-      it 'check that service has errors' do
-        expect(errors).not_to be_nil
-      end
+    describe 'skill' do
+      context 'not enough' do
+        let(:player) { create(:player, gold: 50, lumberjacking_skill: 1) }
 
-      it 'checks that service has particular error' do
-        expect(errors).to include('Level is not enough')
+        it 'check that service failed' do
+          expect(subject.failure?).to eq true
+        end
+
+        it 'check that service has errors' do
+          expect(errors).not_to be_nil
+        end
+
+        it 'checks that service has particular error' do
+          expect(errors).to include('Level or skill is not enough')
+        end
       end
     end
   end
