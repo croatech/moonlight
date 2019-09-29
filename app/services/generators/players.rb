7ef1d0ce79364309dc5657f4366e589498fddcc0
Vitaliy Arnaut <vitaliy.arnaud@gmail.com>
2019-09-29 23:25:09 +0300

    fix player seeds

--- app/services/generators/players.rb
+++ app/services/generators/players.rb
@@ -1,6 +1,6 @@
 module Generators::Players
   def self.call
-    user = User.create!(email: 'croaton6@gmail.com', password: 'password', name: 'Croaton')
-    Player.create(user_id: user.id, current_hp: Player::INITIAL_HP, gold: Player::INITIAL_GOLD, name: user.name, avatar_id: Avatar.first.id, location: Location.first)
+    user = User.create!(email: 'croaton6@gmail.com', password: 'password', username: 'Croaton')
+    Player.create(user_id: user.id, current_hp: Player::INITIAL_HP, gold: Player::INITIAL_GOLD, name: user.username, avatar_id: Avatar.first.id, location: Location.first)
   end
 end
